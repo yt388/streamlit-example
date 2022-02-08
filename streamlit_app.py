c7eb50ccf62e32f345829b0ef9a97ccbce1c1b6e
yt388 <98738680+yt388@users.noreply.github.com>
2022-02-07 22:11:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ chart_data = pd.DataFrame(
 
 st.area_chart(chart_data)
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+chart_data1 = pd.DataFrame(
+     np.random.randn(50, 3),
+     columns=['a', 'b', 'c'])
 
-st.pyplot(fig)
+st.line_chart(chart_data1)
